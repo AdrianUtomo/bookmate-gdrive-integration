15e14acb3546f883471935328a335ba6d39101b9
Adrian Hakim Utomo <adrianhakimutomo@gmail.com>
2025-07-04 11:41:11 +0700

    feat: create google external account if user doesn't have it yet

--- src/lib/reauthAcct.ts
+++ src/lib/reauthAcct.ts
@@ -1,21 +1,32 @@
 export async function reauthAcct(user: any) {
   if (user) {
-      const googleAccount = user.externalAccounts.find(
-          (ea: any) => ea.provider === "google"
-      );
+    const googleAccount = user?.externalAccounts.find(
+      (ea: any) => ea.provider === "google"
+    );
 
-      const reauth = await googleAccount?.reauthorize({
+    if (!googleAccount) {
+      try {
+        await user.createExternalAccount({
+          strategy: "oauth_google",
           redirectUrl: window.location.href,
-          additionalScopes: [
-              "https://www.googleapis.com/auth/drive",
-              "https://www.googleapis.com/auth/calendar.readonly",
-              "https://www.googleapis.com/auth/calendar.events.readonly",
-          ],
-      });
-
-      if (reauth?.verification?.externalVerificationRedirectURL) {
-          window.location.href =
-              reauth?.verification?.externalVerificationRedirectURL.href;
+        });
+      } catch (error) {
+        console.error("Error connecting Google Account", error);
       }
+    }
+
+    const reauth = await googleAccount?.reauthorize({
+      redirectUrl: window.location.href,
+      additionalScopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/calendar.readonly",
+        "https://www.googleapis.com/auth/calendar.events.readonly",
+      ],
+    });
+
+    if (reauth?.verification?.externalVerificationRedirectURL) {
+      window.location.href =
+        reauth?.verification?.externalVerificationRedirectURL.href;
+    }
   }
 }
